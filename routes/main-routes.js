67b62bcc1392e1c905f66b19c932c45ac6611e8f
Vladimir J. Castañeda G <vladimir29castaneda@gmail.com>
2021-06-30 16:17:23 -0300

    test api

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const basePath = '/';
+const db = require('./../database.json');
 
 router.get(basePath, (req, res) => {
-    res.status(200).json('hello wolrd');
+    const filter = req.query
+
+    let result = db.filter(item => {
+        for (let key in filter) {
+            if (item[key] === undefined || item[key] != filter[key])
+                return false;
+        }
+        return true;
+    });
+
+    res.status(200).json(result);
 });
 
 module.exports = router;
