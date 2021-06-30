3605871ed6d16ae26a80aac06a36bdc55c170fe2
Vladimir J. Castañeda G <vladimir29castaneda@gmail.com>
2021-06-30 10:21:00 -0300

    express base

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const server = express();
+const port = 3000;
+
+server.use(cors());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.listen(port, function() {
+    console.log(`server listening on port ${port}`);
+});
